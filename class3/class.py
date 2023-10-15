7125eedec7eafcd8f12ee2a730ca0ab423b3a998
woody0516 <woody20120516@gmail.com>
2023-10-15 11:01:38 +0800

    class3

---
+++ class3/class.py
@@ -0,0 +1,36 @@
+# 字串格式化
+# 使用print印出字串 我是phython,今年31歲
+# name = "Python"
+# age = 10
+# new_str = "我是" + name + "," + "今年" + str(age) + "歲"  # 格式一
+# print(new_str)
+# new_str = "我是%s,今年%d歲了" % (name, age)  # 格式一
+# print(new_str)
+# num = int(input("請輸入一故數字"))
+# total = num + 1
+# print(total)
+# try看看
+# 可以使用try和except來避免城市直接停止運作
+# try:
+#     num = int(input("請輸入一個字"))
+#     total = num + 1
+#     print(total)
+# except:
+#     print("發生錯誤")
+# !=不相等
+# ==相等
+# >=大於等於
+# <=小魚等魚
+# >大於
+# <小魚
+# not都不對
+# and兩個對
+# or其中一個對
+# if 敘述
+# 當使用者輸入密碼時,顯示歡迎光鈴
+password = input("請輸入你的密碼")
+if password == "1234":
+    print("歡迎光零")
+# if else
+else:
+    print(米馬錯誤)
