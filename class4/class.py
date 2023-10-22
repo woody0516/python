f0194ccc20c43d4cf90671aba31942624298bf01
woody0516 <woody20120516@gmail.com>
2023-10-22 10:57:39 +0800

    class.py

---
+++ class4/class.py
@@ -0,0 +1,41 @@
+# password+input('請輸入密碼:')
+# if password=='1234':
+#     print("歡迎光鈴john")
+# elif password=="5678":
+#     print(歡迎ray)
+# else:
+#     print('密碼錯誤')
+# score = int(input("請輸入成績"))
+# if score >= 90:
+#     print("A")
+# if score >= 80 and score <= 89:
+#     print("b")
+# if score >= 70 and score <= 79:
+#     print("c")
+# if score >= 60 and score <= 69:
+#     print("d")
+# if score <= 59:
+# # print("e")
+# word = int(input("請輸入一個數字"))
+# if word % 2 == 1:
+#     print("基數")
+# else:
+#     print("偶數")
+
+
+# for i in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.done()
+# import turtle
+
+# turtle.color("blue")
+# turtle.shape("circle")
+# turtle.penup()
+
+# for i in range(0, 360, 2):
+#     turtle.forward(i)
+#     turtle.right(18)
+#     turtle.stamp()
+
+# turtle.done()
