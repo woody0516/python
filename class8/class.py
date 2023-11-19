4d9e05fa4b71c238d89a2437e99dc360c99d0b96
woody0516 <woody20120516@gmail.com>
2023-11-19 11:01:32 +0800

    class8

---
+++ class8/class.py
@@ -0,0 +1,51 @@
+# weather = ["晴天", "多雲", "雨天", "晴天", "多雲", "雷陣雨", "晴天"]
+# weather[1]
+# weather[2]
+# weather[3]
+# weather[4]
+# weather[5]
+# weather[6]
+# weather[7]
+# n = int(input("請輸入要修改的日期"))
+# a = int(input("請輸入新的天氣"))
+# if a==2:
+#     print["多雲", "多雲", "雨天", "晴天", "多雲", "雷陣雨", "晴天"]
+# if a==3:
+#     print["多雲", "多雲", "雨天", "晴天", "多雲", "雷陣雨", "晴天"]
+# if a==4:
+#     print["多雲", "多雲", "雨天", "晴天", "多雲", "雷陣雨", "晴天"]
+# if a==5:
+#     print["多雲", "多雲", "雨天", "晴天", "多雲", "雷陣雨", "晴天"]
+# if a==6:
+#     print["多雲", "多雲", "雨天", "晴天", "多雲", "雷陣雨", "晴天"]
+# false
+# weather = ["晴天", "多雲", "雨天", "晴天", "多雲", "雷陣雨", "晴天"]
+# print(weather)
+# while True:
+#     try:
+#         ans = int(input("請輸入要修改的日期(1~7):"))
+#     except:
+#         print("請輸入數字編號")
+#         continue
+
+#     if ans > len(weather) or ans < 1:
+#         print("請輸入錯誤查無此星期,請崇興輸入")
+#     else:
+#         print("請輸入要修改的日期" + str(ans))
+#         print("原本的天氣是" + [ans - 1])
+#         new_weather = input("請輸入新的天氣")
+#         print("修改的後的天氣+weather[ans-1]")
+#         print(weather)
+#         break
+# l=['a,b,c']
+# a=l.copy()
+# a[0]=1
+# print(a,l)
+# a = ["牛奶", "麵包"]
+# b = ["貧果", "香蕉"]
+# print(a + b)
+[1, 2] * 2
+fruit_list = ["「火龍果」", "「哈密瓜」", "「百香果」", "「橘子」", "「蘋果」", "「香蕉」", "「梨」", "「李」", "「桃」"]
+print("最長的水果" + max(fruit_list))
+print("最短的水果" + min(fruit_list))
+list(range(3))
---
+++ class8/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+weather = ["晴天", "多雲", "雨天", "晴天", "多雲", "雷陣雨", "晴天"]
+# print(weather)
+# while True:
+#     try:
+#         ans = int(input("請輸入要修改的日期(1~7):"))
+#     except:
+#         print("請輸入數字編號")
+#         continue
+
+#     if ans > len(weather) or ans < 1:
+#         print("請輸入錯誤查無此星期,請崇興輸入")
+#     else:
+#         print("請輸入要修改的日期" + str(ans))
+#         print("原本的天氣是" + [ans - 1])
+#         new_weather = input("請輸入新的天氣")
+#         print("修改的後的天氣+weather[ans-1]")
+#         print(weather)
+#         break
